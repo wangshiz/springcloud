c15beba99cf70eace10a1c815a5f0145521b4115
wangshiz <312406078@qq.com>
2019-10-12 16:27:16 +0800

    feign添加断路器 新增zuul

--- service-feign/src/main/java/cn/wsz/servicefeign/ServiceFeignApplication.java
+++ service-feign/src/main/java/cn/wsz/servicefeign/ServiceFeignApplication.java
@@ -1,17 +1,44 @@
 package cn.wsz.servicefeign;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrixDashboard
 public class ServiceFeignApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceFeignApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+
     }
 
 }
